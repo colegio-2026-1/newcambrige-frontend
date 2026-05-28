fb5e4a0d3f4f5d0fcde63c4939c2f30cf75b2793
brahayan32 <brahayancampo32@gmail.com>
2026-05-28 13:20:03 -0500

    feat: configurar servicios y endpoints de Banda
    Implementación de la capa de servicios y rutas de API para el módulo de Banda.

---
+++ src/api/bandaService.js
@@ -0,0 +1,27 @@
+import axiosClient from "./axiosClient";
+import { endpoints } from "./endpoints";
+
+const { BANDA } = endpoints;
+
+export const bandaService = {
+  // Categorías
+  getCategorias: () => axiosClient.get(BANDA.categorias),
+
+  // Ubicaciones
+  getUbicaciones: () => axiosClient.get(BANDA.ubicaciones),
+
+  // Instrumentos
+  getInstrumentos: () => axiosClient.get(BANDA.instrumentos),
+  getInstrumentosDisponibles: () => axiosClient.get(BANDA.instrumentosDisponibles),
+  crearInstrumento: (data) => axiosClient.post(BANDA.instrumentos, data),
+  editarInstrumento: (id, data) => axiosClient.put(BANDA.instrumentoById(id), data),
+  eliminarInstrumento: (id) => axiosClient.delete(BANDA.instrumentoById(id)),
+
+  // Préstamos
+  getPrestamos: () => axiosClient.get(BANDA.prestamos),
+  crearPrestamo: (data) => axiosClient.post(BANDA.prestamos, data),
+  devolverInstrumento: (id) => axiosClient.put(BANDA.devolverPrestamo(id)),
+
+  // Estadísticas
+  getEstadisticas: () => axiosClient.get(BANDA.estadisticas),
+};
