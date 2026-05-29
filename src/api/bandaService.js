50d47644768d1a3888c9d107ce4bbc632cc1a92f
brahayan32 <brahayancampo32@gmail.com>
2026-05-29 09:01:17 -0500

    feat: implementacion 50%

--- src/api/bandaService.js
+++ src/api/bandaService.js
@@ -4,24 +4,106 @@ import { endpoints } from "./endpoints";
 const { BANDA } = endpoints;
 
 export const bandaService = {
-  // Categorías
-  getCategorias: () => axiosClient.get(BANDA.categorias),
 
-  // Ubicaciones
-  getUbicaciones: () => axiosClient.get(BANDA.ubicaciones),
+  // =====================================================
+  // CATEGORIAS
+  // =====================================================
 
-  // Instrumentos
-  getInstrumentos: () => axiosClient.get(BANDA.instrumentos),
-  getInstrumentosDisponibles: () => axiosClient.get(BANDA.instrumentosDisponibles),
-  crearInstrumento: (data) => axiosClient.post(BANDA.instrumentos, data),
-  editarInstrumento: (id, data) => axiosClient.put(BANDA.instrumentoById(id), data),
-  eliminarInstrumento: (id) => axiosClient.delete(BANDA.instrumentoById(id)),
+  getCategorias: () =>
 
-  // Préstamos
-  getPrestamos: () => axiosClient.get(BANDA.prestamos),
-  crearPrestamo: (data) => axiosClient.post(BANDA.prestamos, data),
-  devolverInstrumento: (id) => axiosClient.put(BANDA.devolverPrestamo(id)),
+    axiosClient.get(
+      BANDA.categorias
+    ),
 
-  // Estadísticas
-  getEstadisticas: () => axiosClient.get(BANDA.estadisticas),
+  // =====================================================
+  // UBICACIONES
+  // =====================================================
+
+  getUbicaciones: () =>
+
+    axiosClient.get(
+      BANDA.ubicaciones
+    ),
+
+  // =====================================================
+  // ESTUDIANTES
+  // =====================================================
+
+  getEstudiantes: () =>
+
+    axiosClient.get(
+      "/api/estudiantes/"
+    ),
+
+  // =====================================================
+  // INSTRUMENTOS
+  // =====================================================
+
+  getInstrumentos: () =>
+
+    axiosClient.get(
+      BANDA.instrumentos
+    ),
+
+  getInstrumentosDisponibles: () =>
+
+    axiosClient.get(
+      BANDA.instrumentosDisponibles
+    ),
+
+  crearInstrumento: (data) =>
+
+    axiosClient.post(
+      BANDA.instrumentos,
+      data
+    ),
+
+  editarInstrumento: (
+    id,
+    data
+  ) =>
+
+    axiosClient.put(
+      BANDA.instrumentoById(id),
+      data
+    ),
+
+  eliminarInstrumento: (id) =>
+
+    axiosClient.delete(
+      BANDA.instrumentoById(id)
+    ),
+
+  // =====================================================
+  // PRESTAMOS
+  // =====================================================
+
+  getPrestamos: () =>
+
+    axiosClient.get(
+      BANDA.prestamos
+    ),
+
+  crearPrestamo: (data) =>
+
+    axiosClient.post(
+      BANDA.prestamos,
+      data
+    ),
+
+  devolverPrestamo: (id) =>
+
+    axiosClient.put(
+      BANDA.devolverPrestamo(id)
+    ),
+
+  // =====================================================
+  // ESTADISTICAS
+  // =====================================================
+
+  getEstadisticas: () =>
+
+    axiosClient.get(
+      BANDA.estadisticas
+    ),
 };
