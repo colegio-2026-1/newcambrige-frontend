b20d12f3f95a3a2ca6ea01d5bd73ea132b997bb0
rdoskip <ronaldsantiagoninotineo@gmail.com>
2026-06-04 23:30:51 -0500

    cambios necesarios para despliegue
    Linux si reconoce mayusculas y minusculas | windows no

--- src/modules/salon/PupitrePage.jsx
+++ src/modules/salon/PupitrePage.jsx
@@ -16,7 +16,7 @@ import { allrolesuserRequest, allaniosacademicosRequest } from "../../api/endpoi
 import Header         from "../../components/layout/header";
 import ModuleLayout   from "../../components/layout/ModuleLayout";
 import Sidebar        from "../../components/layout/Sidebar";
-import SearchBar      from "../../components/shared/SearchBar";
+import SearchBar      from "../../components/shared/searchBar";
 import DataTable      from "../../components/shared/DataTable";
 import ActionButtons  from "../../components/shared/ActionButtons";
 import Modal          from "../../components/shared/Modal";
