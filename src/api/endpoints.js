016e3e971996298c7ccf7ac806dade59f0504bd8
brahayan32 <brahayancampo32@gmail.com>
2026-06-05 00:01:13 -0500

    feat: separación rutas

--- src/api/endpoints.js
+++ src/api/endpoints.js
@@ -1,11 +1,4 @@
-
 import axiosClient from "./axiosClient";
 
 export const allrolesuserRequest = (id) => axiosClient.get(`/api/usuarios/${id}/roles`);
 export const allaniosacademicosRequest =() => axiosClient.get("/api/parametrizacion/anio-escolar");
-
-export const allsalonesRequest = () => axiosClient.get("/api/salones/");
-export const allmatriculasRequest = () => axiosClient.get("/api/secretaria/matriculas/");
-export const allaniosacademicosRequest =() => axiosClient.get("/api/parametros/periodos");
-export const crearMatriculaRequest = ({ estudiante_id, periodo_id }) => axiosClient.post(`/api/tesoreria/registrar-pago?estudiante_id=${estudiante_id}&periodo_id=${periodo_id}`, null);
-
